bbd2b080fcc2d990ff2709641a68bf992a849e41
beek <31266789+binzh303@users.noreply.github.com>
2022-09-03 20:29:13 +0800

    Delete src/main/java/cn/javatip/vinfo directory

--- src/main/java/cn/javatip/vinfo/mapper/DataMapper.java
+++
@@ -1,7 +0,0 @@
-package cn.javatip.vinfo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import cn.javatip.vinfo.bean.DataBean;
-
-public interface DataMapper extends BaseMapper<DataBean> {
-}
--- src/main/java/cn/javatip/vinfo/service/DataService.java
+++
@@ -1,8 +0,0 @@
-package cn.javatip.vinfo.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import cn.javatip.vinfo.bean.DataBean;
-
-public interface DataService extends IService<DataBean> {
-
-}
